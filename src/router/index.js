4e52ad205d1f728ba30eb6cb6c8ea74f8a7a6f15
levo <zhangtao13140725@163.com>
2017-07-11 15:15:53 +0800

    路由整理

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import App from '../App'
 
 
 // 主页
-const mainWork = r => require.ensure([], () => r(require('../pages/main/index.vue')),'mainWork')
+const index = r => require.ensure([], () => r(require('../pages/main/index.vue')),'index')
 //滑屏主页
 const mainWorks = r => require.ensure([], () => r(require('../pages/touchMain/index.vue')),'mainWorks')
 
@@ -14,20 +14,16 @@ Vue.use(Router)
 const routes = [{
   path:'/',component:App,
   children:[
-    { path: "",redirect:"/mainWork" },
-    { path: "/mainWork",component: mainWork},
+    { path: "",redirect:"/index" },
+    { path: "/index",component: index},
     { path: "/mainWorks",component: mainWorks},
-    //我的页面 的子路由
-    { path: "/mainMy",component:routerChild,
-      children:[
-        {path:'/',component:mainWork},
-      ]
-    },
+    // 采购商
     { path: "/buyer",component:routerChild,
       children:[
         {path:'',component:mainWork,meta:{requiresAuth:true,uid:1}},
       ]
     },
+    // 供应商
     { path: "/suplier",component:routerChild,
       children:[
         {path:'',component:mainWork,meta:{requiresAuth:true,uid:0}},
